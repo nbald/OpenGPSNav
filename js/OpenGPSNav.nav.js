4153219d06d1efb8fcc12a54204eb67d96337c4d
Nicolas BALDECK <nicolas.baldeck@romma.fr>
2013-10-04 20:27:50 +0200

    ajout osrm

--- js/OpenGPSNav.nav.js
+++ js/OpenGPSNav.nav.js
@@ -39,11 +39,40 @@ OGN.nav.go = function (lat, lon) {
 
 OGN.nav.showMap = function () {
 	$( "body" ).load( "html/navigation.html", function () {
+
+
+		console.log(OGN.nav.route);
+		var path_coordinates = OSRM.RoutingGeometry._decode(OGN.nav.route.route_geometry, 5);
+		console.log(path_coordinates);
+
+
+		var vector = new ol.layer.Vector({
+			 source: new ol.source.Vector({
+			 	data: {
+				  "type": "FeatureCollection",
+				  "features": [
+				    {
+				      "type": "Feature",
+				      "properties": { 'color': '#BADA55' },
+				      "geometry": {
+				        "type": "LineString",
+				        "coordinates": path_coordinates
+				      }
+				    }
+				  ]
+				},
+			parser: new ol.parser.GeoJSON(),
+			projection: ol.proj.get('EPSG:4326')
+			}),
+		});		
+
 		OGN.nav.map = new ol.Map({
 			target: 'map',
-			layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
-			controls: [],
+			layers: [vector, new ol.layer.Tile({source: new ol.source.OSM()})],
+			controls: ol.control.defaults({attribution: false}),
+ 			renderer: ol.RendererHint.CANVAS,
 			view: new ol.View2D({
+				projection: 'EPSG:3857',
 				center: ol.proj.transform([OGN.nav.start.lon, OGN.nav.start.lat], 'EPSG:4326', 'EPSG:3857'),
 				zoom: 17
 			})
